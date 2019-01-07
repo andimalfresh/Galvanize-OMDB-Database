5038a79915958cdb4f6dc5e407818b499db71873
Andrew Pedersen <w.andrew.pedersen@gmail.com>
2019-01-07 12:40:39 -0700

    Added Error Handling

--- app.js
+++ app.js
@@ -13,16 +13,19 @@ app.get('/', (req,res) => {
     queries.listAll().then(movies => res.json(movies))
 })
 app.get('/:id', (req,res) => {
-    queries.listById(req.params.id).then(movies => res.send(movies))
+    queries.listById(req.params.id).then(movies => res.json(movies))
 })
 app.post('/', (req, res) => {
-    queries.createMovie(req.body).then(res.sendStatus(201))
+    queries.createMovie(req.body).then(movies => res.send(movies))
 })
 app.delete('/:id', (req,res) => {
-    queries.deleteMovie(req.params.id).then(res.sendStatus(204))
+    queries.deleteMovie(req.params.id).delete().then(res.sendStatus(204))
 })
 app.put('/:id', (req,res) => {
     queries.updateMovie(req.params.id, req.body).then(movies => res.json(movies))
 })
-
+app.use((err, req, res, next) => {
+    const status = err.status || 500
+    res.status(status).json({ error: err})
+})
 app.listen(port, console.log(`We are going deep on PORT: ${port}`))
